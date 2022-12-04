ec2bc579175d98d394682b9eeb58a1d10b86b8a3
hyeonwoojo <h_jo@ejn.gg>
2022-12-04 19:40:37 +0900

    feat: 메인 페이지 디자인 완성

--- components/Welcome/Welcome.styles.ts
+++ components/Welcome/Welcome.styles.ts
@@ -3,12 +3,30 @@ import { createStyles } from '@mantine/core';
 export default createStyles((theme) => ({
   title: {
     color: theme.colorScheme === 'dark' ? theme.white : theme.black,
-    fontSize: 100,
+    fontSize: 65,
     fontWeight: 900,
     letterSpacing: -2,
 
     [theme.fn.smallerThan('md')]: {
       fontSize: 50,
+    },
+
+    [theme.fn.smallerThan('xs')]: {
+      fontSize: 30,
+      marginTop: 30,
+    },
+  },
+  center: {
+    [theme.fn.smallerThan('xs')]: {
+      marginLeft: 4,
+      marginRight: 4,
+    },
+  },
+  br: {
+    display: 'block',
+
+    [theme.fn.smallerThan('xs')]: {
+      display: 'none',
     },
   },
 }));
--- components/Welcome/Welcome.tsx
+++ components/Welcome/Welcome.tsx
@@ -1,4 +1,16 @@
-import { Title, Text, Anchor } from '@mantine/core';
+import {
+  Title,
+  Text,
+  Anchor,
+  Container,
+  Flex,
+  Paper,
+  TextInput,
+  PasswordInput,
+  Group,
+  Checkbox,
+  Button,
+} from '@mantine/core';
 import useStyles from './Welcome.styles';
 
 export function Welcome() {
@@ -6,19 +18,50 @@ export function Welcome() {
 
   return (
     <>
-      <Title className={classes.title} align="center" mt={100}>
-        Welcome to{' '}
-        <Text inherit variant="gradient" component="span">
-          Mantine
-        </Text>
-      </Title>
+      <Container size="lg">
+        <Flex
+          justify="space-around"
+          align="center"
+          direction={{ base: 'column', xs: 'row' }}
+          my={50}
+        >
+          <Title className={classes.title} align="left">
+            AfreecaTV
+            <br className={classes.br} />
+            <Text inherit variant="gradient" component="span" className={classes.center}>
+              Discord
+            </Text>
+            <br className={classes.br} />
+            Alert
+          </Title>
+          <Paper withBorder shadow="md" p={30} my={30} radius="md" style={{ minWidth: 300 }}>
+            <TextInput label="아이디" placeholder="아이디를 입력해주세요" required />
+            <PasswordInput
+              label="비밀번호"
+              placeholder="비밀번호를 입력해주세요"
+              required
+              mt="md"
+            />
+            <Group position="apart" mt="lg">
+              <Checkbox label="로그인 유지" sx={{ lineHeight: 1 }} />
+              <Anchor<'a'> onClick={(event) => event.preventDefault()} href="#" size="sm">
+                아이디/비밀번호 찾기
+              </Anchor>
+            </Group>
+            <Group position="apart" noWrap>
+              <Button fullWidth mt="xl">
+                로그인
+              </Button>
+              <Button fullWidth mt="xl" variant="outline">
+                회원가입
+              </Button>
+            </Group>
+          </Paper>
+        </Flex>
+      </Container>
       <Text color="dimmed" align="center" size="lg" sx={{ maxWidth: 580 }} mx="auto" mt="xl">
-        This starter Next.js project includes a minimal setup for server side rendering, if you want
-        to learn more on Mantine + Next.js integration follow{' '}
-        <Anchor href="https://mantine.dev/guides/next/" size="lg">
-          this guide
-        </Anchor>
-        . To get started edit index.tsx file.
+        이제 디스코드로도 아프리카TV 방송 알림을 받을 수 있습니다. 간편하게 회원가입하고 무료로
+        제공되는 알림 서비스를 경험해보세요.{' '}
       </Text>
     </>
   );
